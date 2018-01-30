213c37c2fba1b99ec001e20cb02afcb661a483dd
getmaxed <maxfaden07@gmail.com>
2018-01-30 11:18:06 -0800

    Method to show alert message created

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const github = new Github;
+/*
+|--------------------------------------------------------------------------
+| Init UI & Github
+|--------------------------------------------------------------------------
+*/
 
+const github = new Github;
 const ui = new UI();
+
+/*
+|--------------------------------------------------------------------------
+| Search users
+|--------------------------------------------------------------------------
+*/
 
 const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup', (e) => {
@@ -11,12 +22,14 @@ searchUser.addEventListener('keyup', (e) => {
         .then(data => {
             if (data.profile.message === 'Not Found') {
                 //show alert
+                ui.showAlert('User not found', 'alert alert-danger');
             } else {
                 ui.showProfile(data.profile);
             }
         })
     } else {
         //clear profile
+        ui.clearProfile();
     }
 
 });
