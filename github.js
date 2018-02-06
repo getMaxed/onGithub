3138955e0d14fb4e812ffc149f25d63f8577a9eb
getmaxed <maxfaden07@gmail.com>
2018-02-05 21:47:39 -0800

    Method to fetch and display repos created

--- github.js
+++ github.js
@@ -2,15 +2,20 @@ class Github {
     constructor() {
         this.client_id = '1ab9c378c5422f89c1a9';
         this.client_secret = '565a2326ddce9c9f6c42312cc44dd2c181034bb8';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-            profile
-
+            profile,
+            repos
         }
     }
 }
